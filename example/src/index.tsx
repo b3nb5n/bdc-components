1d0a718d2ce0ab269ce344b0ed6e6f0fe3124a36
Ben Baldwin <benbaldwindesign@gmail.com>
2020-09-05 23:52:27 -0700

    Clean up example environment

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,7 +1,32 @@
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { FormModal } from 'bdc-components';
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+const App = () => {
+	return (
+		<FormModal
+			name="Sign Up"
+			fieldStructures={[
+				{ type: 'text', name: 'name', helpText: 'is this helpful', required: true },
+				{ type: 'date', name: 'dob' },
+				{
+					type: 'option',
+					name: 'favorite food',
+					options: [ 'pizza', 'salad', 'cookies', 'macaroni', 'chicken' ],
+					multi: true
+				},
+				{
+					type: 'file',
+					name: 'headshot',
+					helpText: 'is this helpful?',
+					required: true
+				},
+				{ type: 'text', name: 'bio', multiline: true }
+			]}
+			handleSubmit={console.log}
+			handleClose={() => console.log('close')}
+		/>
+	);
+};
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'));
