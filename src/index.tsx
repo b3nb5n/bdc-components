a2f1ce1790d8babdd554ec32fd441a893fb203c7
Ben Baldwin <benbaldwindesign@gmail.com>
2020-09-13 19:57:19 -0700

    included initialValue function in exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 export * from './theme/bdc-theme';
 export * from './theme/theme';
 export * from './form modal/form-modal';
+export * from './form modal/initial-state';
 export * from './page header/page-header';
 export * from './button/button';
